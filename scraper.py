2103232f2cb41ffbd1a1a5423f1db41790c51a93
woodbine <ian@spendnetwork.com>
2017-05-03 11:29:56 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -86,7 +86,13 @@ def convert_mth_strings ( mth_string ):
 #### VARIABLES 1.0
 
 entity_id = "E4203_MCC_gov"
-urls = ['http://www.manchester.gov.uk/open/downloads/download/33/expenditure_exceeding_500_2015_2016', 'http://www.manchester.gov.uk/open/downloads/73/archive', 'http://www.manchester.gov.uk/open/downloads/44/e']
+urls = ['http://www.manchester.gov.uk/open/downloads/75/expenditure_exceeding_500_2010_2011',
+        'http://www.manchester.gov.uk/open/downloads/76/expenditure_exceeding_500_2011_2012',
+        'http://www.manchester.gov.uk/open/downloads/77/expenditure_exceeding_500_2012_2013',
+        'http://www.manchester.gov.uk/open/downloads/78/expenditure_exceeding_500_2013_2014',
+        'http://www.manchester.gov.uk/open/downloads/79/expenditure_exceeding_500_2014_2015',
+        'http://www.manchester.gov.uk/open/downloads/93/expenditure_exceeding_500_2015_2016',
+        'http://www.manchester.gov.uk/open/downloads/44/e']
 errors = 0
 data = []
 url = 'http://example.com'
@@ -121,20 +127,21 @@ for url in urls:
             blocklinks = block.find_all('div', 'download_indent')
             for blocklink in blocklinks:
                 for link in blocklink.find_all('a'):
-                    text_link = ''
-                    try:
-                        text_link = link.text
-                    except: pass
-                    if text_link:
-                        csvfiles_html = urllib2.urlopen(link['href'])
-                        sp = BeautifulSoup(csvfiles_html, 'lxml')
-                        blocks_download = sp.find('div', 'download_indent')
-                        url = blocks_download.find('h4').find('a')['href']
-                        csvMth = blocks_download.find('h4').find('a').text.split(' ')[-2][:3]
-                        csvYr = blocks_download.find('h4').find('a').text.split(' ')[-1]
-                        csvMth = convert_mth_strings(csvMth.upper())
-                        todays_date = str(datetime.now())
-                        data.append([csvYr, csvMth, url])
+                    if 'Metadata' not in link.text:
+                        text_link = ''
+                        try:
+                            text_link = link.text
+                        except: pass
+                        if text_link:
+                            csvfiles_html = urllib2.urlopen(link['href'])
+                            sp = BeautifulSoup(csvfiles_html, 'lxml')
+                            blocks_download = sp.find('div', 'download_indent')
+                            url = blocks_download.find('h4').find('a')['href']
+                            csvMth = blocks_download.find('h4').find('a').text.split(' ')[-2][:3]
+                            csvYr = blocks_download.find('h4').find('a').text.split(' ')[-1]
+                            csvMth = convert_mth_strings(csvMth.upper())
+                            todays_date = str(datetime.now())
+                            data.append([csvYr, csvMth, url])
         except: pass
 
 
@@ -160,4 +167,3 @@ if errors > 0:
 
 
 #### EOF
-
